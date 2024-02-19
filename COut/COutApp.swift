9bc8b2503dbb90565563100e7599749e1cd54975
Aadithya-J <jlaadithya@gmail.com>
2024-02-19 18:52:04 +0530

    Initial Commit

---
+++ COut/COutApp.swift
@@ -0,0 +1,17 @@
+//
+//  COutApp.swift
+//  COut
+//
+//  Created by Aadithya Jayakaran on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct COutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
