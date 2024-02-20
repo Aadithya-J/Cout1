42f3056a6b751b30acee3a7cdbe930b66376e52a
Aadithya-J <jlaadithya@gmail.com>
2024-02-20 13:20:29 +0530

    Added

--- COut/COutApp.swift
+++ COut/COutApp.swift
@@ -1,17 +1,31 @@
-//
-//  COutApp.swift
-//  COut
-//
-//  Created by Aadithya Jayakaran on 19/02/24.
-//
-
 import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
 
+class AppDelegate: NSObject,UIApplicationDelegate {
+    func application(_ application: UIApplication,didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        let db = Firestore.firestore()
+        FirestoreManager.shared.db = db
+        return true
+    }
+}
+
+class FirestoreManager {
+    static let shared = FirestoreManager()
+    var db: Firestore!
+
+    func accessFirestore() -> Firestore {
+        return db
+    }
+}
 @main
 struct COutApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AuthViewController()
         }
     }
 }
